990cdf43e020a1e63ccdab80258986759a72ae39
Merabi-Giorgadze <124910119+Merabi-Giorgadze@users.noreply.github.com>
2025-05-02 16:55:45 +0400

    Fourth commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -78,7 +78,6 @@ questions.forEach((btn) => {
 
 document.querySelectorAll('.home-link ').forEach(link => {
     link.addEventListener('click', function (e) {
-        e.preventDefault();
         const targetId = this.getAttribute('href').substring(1);
         const targetElement = document.getElementById(targetId);
 
